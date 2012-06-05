7cbe1117ea2b5aa08862006768c2a545d3878b89
Joshua Pekera <joshua@smagency.com>
2012-06-05 18:31:32 -0500

    New theme added
    New theme added
    
    Signed-off-by: Joshua Pekera <joshua@smagency.com>

---
+++ resume/theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Resume extends Theme
+{
+    public $name			= 'Resume';
+    public $author			= 'Joshua Pekera';
+    public $author_website	= 'http://joshuapekera.com';
+    public $website			= 'http://luckie.joshuapekera.com';
+    public $description		= 'A responsive resume site.';
+    public $version			= '1.0';
+}
+
+/* End of file theme.php */
